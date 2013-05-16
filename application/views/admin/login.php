80839b4c554056ff2221d857813f23571c4750b7
Marko Aleksic <psybaron@gmail.com>
2013-05-16 13:37:41 +0200

    legacy code delete

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,14 +1,3 @@
-<!-- <div id="adminLogin">
-	<h4><?php echo $G_login;?></h4>
-	<hr/>
-	<?php echo form_open('admin/login'); ?>
-		<p><?php echo form_label('Username','username');?></p>
-		<p><?php echo form_input('username','',"id='username'"); ?></p>
-		<p><?php echo form_label('Password','password');?></p>
-		<p><?php echo form_password('password','',"id='password'"); ?></p>
-		<p><?php echo form_submit('','Login'); ?>
-	<?php echo form_close(); ?>
-</div> -->
 <div id="login" class="well">
 	<?php echo form_open('admin/login','class="form-horizontal"'); ?>
 		<fieldset>
