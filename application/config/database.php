d88fc19d6cc3a69268facd23222d814f8e4a93cc
Marko Aleksic <psybaron@gmail.com>
2013-06-03 09:32:49 +0200

    new files uploaded

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'gradska_pekara_pms';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'gpekara_pms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
